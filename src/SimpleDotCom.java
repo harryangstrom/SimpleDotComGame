40466788d4cf686547c90d07b6c432a13b70bb72
harryangstrom <harry.angstrom67@gmail.com>
2019-04-30 20:26:42 +0200

    Primera version con ArrayList

---
+++ src/SimpleDotCom.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class SimpleDotCom {
+
+    //private int[] location;
+    private ArrayList<Integer> location = new ArrayList<>();
+    private int hits = 0;
+    private int value;
+    private Integer cellToRemove;
+
+    String checkYourself(String guess) {
+        try {
+            value = Integer.parseInt(guess);
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException: " + e);
+            return ("NotNumber");
+        }
+
+        boolean hit = false;
+        for (Integer cell : location) {
+            if (value == cell) {
+                hits++;
+                cellToRemove = cell;
+                hit = true;
+            }
+        }
+        if (!hit) {
+            return ("miss");
+        } else {
+            location.remove(cellToRemove);
+            System.out.println("Localización: " + location.toString());
+            if (location.isEmpty()) {
+                return ("kill");
+            } else {
+                return ("hit");
+            }
+        }
+    }
+
+    void setLocationCells(ArrayList<Integer> location) {
+        this.location = location;
+    }
+}
+
---
+++ src/SimpleDotComGame.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        final int MAX = 5;
+        int numberOfGuesses = 0;
+        SimpleDotCom dot = new SimpleDotCom();
+        GameHelper helper = new GameHelper();
+        int startLocation = (int) Math.floor(Math.random() * MAX + 1);
+        ArrayList<Integer> location = new ArrayList<>();
+        location.add(startLocation);
+        location.add(startLocation + 1);
+        location.add(startLocation + 2);
+        //int[] cells = {startLocation, startLocation + 1, startLocation + 2};
+        System.out.println("Localización: " + location.toString());
+        //System.out.println("Localización: " + Arrays.toString(cells));
+        //dot.setLocationCells(cells);
+        dot.setLocationCells(location);
+        boolean isAlive = true;
+
+        while (isAlive) {
+            String guess = helper.getUserInput("Enter a number:");
+            String result = dot.checkYourself(guess);
+            System.out.println("you've got a " + result);
+            if (!result.equals("NotNumber")) {
+                numberOfGuesses++;
+            }
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " + numberOfGuesses + " guesses.");
+            }
+        }
+
+    }
+}
